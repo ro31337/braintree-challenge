25f66fc892387082db6cc955d61924aa87bbc3fd
Roman Pushkin <roman.pushkin@gmail.com>
2019-02-02 22:54:11 -0800

    Add parser spec

---
+++ spec/parser_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2019 Roman Pushkin
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the 'Software'), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+require_relative '../objects/parser'
+
+describe Parser do
+  subject { Parser.new }
+
+  it 'should parse lines' do
+    expect(subject.parse('Add foo 12345 $123').verb).to eq(:add)
+    expect(subject.parse('Charge foo $123').verb).to eq(:charge)
+    expect(subject.parse('Credit foo $123').verb).to eq(:credit)
+  end
+end
